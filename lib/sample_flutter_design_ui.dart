9d4057ef1efc9dc134be5ab0680e1d03e10f5f56
Pablo Satler <pablosatler@gmail.com>
2022-04-26 13:55:55 -0300

    feat: create input field

--- lib/sample_flutter_design_ui.dart
+++ lib/sample_flutter_design_ui.dart
@@ -1,3 +1,7 @@
 library sample_flutter_design_ui;
 
+// all widgets
 export 'src/widgets/widgets.dart';
+
+// colors
+export 'src/shared/app_colors.dart';
--- lib/src/widgets/design_input_field.dart
+++ lib/src/widgets/design_input_field.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
 
+import 'package:sample_flutter_design_ui/src/shared/app_colors.dart';
+
+// https://stackoverflow.com/questions/59770757/not-possible-to-extend-textfield-in-flutter
+
 class DesignInputField extends StatelessWidget {
-  const DesignInputField({Key? key}) : super(key: key);
+  DesignInputField({
+    Key? key,
+    required this.textEditingController,
+    this.placeholder = '',
+    this.leading,
+    this.trailing,
+    this.trailingTapped,
+    this.isPassword = false,
+  }) : super(key: key);
+
+  final TextEditingController textEditingController;
+  final String placeholder;
+  final Widget? leading;
+  final Widget? trailing;
+  final void Function()? trailingTapped;
+  final bool isPassword;
+
+  final circularBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(8.0),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextField(
+      controller: textEditingController,
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        hintText: placeholder,
+        prefixIcon: leading,
+        suffixIcon: trailing == null
+            ? null
+            : GestureDetector(
+                onTap: trailingTapped,
+                child: trailing,
+              ),
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 15,
+          horizontal: 20,
+        ),
+        filled: true,
+        fillColor: kcVeryLightGreyColor,
+        border: circularBorder.copyWith(
+          borderSide: const BorderSide(color: kcLightGreyColor),
+        ),
+        errorBorder: circularBorder.copyWith(
+          borderSide: const BorderSide(color: Colors.red),
+        ),
+        focusedBorder: circularBorder.copyWith(
+          borderSide: const BorderSide(color: kcPrimaryColor),
+        ),
+        enabledBorder: circularBorder.copyWith(
+          borderSide: const BorderSide(color: kcLightGreyColor),
+        ),
+      ),
+    );
   }
 }
